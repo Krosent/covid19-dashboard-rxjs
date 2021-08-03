6db7c2944fb0e42416d8e6d8466cfdf781b82abb
Krosent <krosent2@gmail.com>
2021-08-04 01:44:31 +0400

    Code refactoring

--- js/core/app.js
+++ js/core/app.js
@@ -26,82 +26,12 @@ const {
     toArray
 } = rxjs.operators
 
-const { webSocket } = rxjs.webSocket
-
-const subject = webSocket("wss://ws-feed.pro.coinbase.com")
-const [one, two, three, four] = subject.pipe(partition(data => data.product_id))
-const currencies = ['BTC-USD', 'LTC-USD', 'DOGE-USD'] // List of currencies
-
-function subscribeOnCurrencies() {
-    // listener
-    subject.subscribe(
-        msg => {
-            console.log('message received: ' + JSON.stringify(msg))
-            let currencyName = msg.product_id.substr(0, msg.product_id.indexOf('-'))
-            pushValueToDashboardChart(chart, msg.price, currencyName)
-        },
-        err => console.log(err),
-        () => console.log('complete')
-    )
-}
-
-function pushMessageToWSServer(currencies) {
-    // request
-    subject.next({
-        "type": "subscribe",
-        "product_ids": currencies,
-        "channels": [
-            {
-                "name": "ticker",
-                "product_ids": currencies
-            }
-        ]
-    })
-}
-
-function pushValueToDashboardChart(chart, price, currencyValue) {
-    const dataset = getDatasetByCurrencyName(chart, currencyValue)
-    if (typeof dataset !== "undefined") {
-        updateChart(chart, dataset, price)
-    }
-}
-
 console.log("Chart initialization step")
-// TODO: Implement
 let chart = createChart() // chart is a global variable in our application
-
-/*console.log("Subscribe on all currencies. Default behavior")
-subscribeOnCurrencies()
-console.log("Push message to the WS that we want this kind of data")
-pushMessageToWSServer(currencies)
-*/
 
 const numOfDays = 30
 let src = `https://corona.lmao.ninja/v2/historical?lastdays=${numOfDays}`
 let historicalData = "https://corona.lmao.ninja/v2/historical/all"
-
-// test 
-function subscribeCovid() {
-    let result = rxjs.from(fetch(src)
-        .then((response) => { return response.json() }))
-        .pipe(
-            switchMap((country) => country),
-            map(elem => [{ countryName: elem }]))
-    result.subscribe(
-        response => console.log(response), e => console.error(e)
-    )
-}
-
-function currenciesList() {
-    let result = rxjs.from(fetch(src).then((response) => { return response.json() }))
-    result.subscribe(response => {
-        // resp = response.map(country => {})
-        console.log(response)
-    }, e => console.error(e))
-}
-
-currenciesList()
-//subscribeCovid()
 
 function loadGlobalStats() {
     let result = rxjs.from(data)
