188dd545ddffcbb3bb899dde5e8c0c0e0862dc8b
Krosent <krosent2@gmail.com>
2021-08-03 23:45:30 +0400

    Country selector, overall stats

--- js/core/app.js
+++ js/core/app.js
@@ -8,6 +8,7 @@ const {
     of,
     range,
     fromEvent,
+    fromPromise
 } = rxjs
 const {
     map,
@@ -15,7 +16,12 @@ const {
     switchMap,
     scan,
     groupBy,
-    partition
+    partition,
+    flatMap,
+    mergeMap,
+    reduce,
+    last,
+    distinct
 } = rxjs.operators
 
 const { webSocket } = rxjs.webSocket
@@ -32,8 +38,8 @@ function subscribeOnCurrencies() {
             let currencyName = msg.product_id.substr(0, msg.product_id.indexOf('-'))
             pushValueToDashboardChart(chart, msg.price, currencyName)
         },
-        err => console.log(err), 
-        () => console.log('complete') 
+        err => console.log(err),
+        () => console.log('complete')
     )
 }
 
@@ -53,14 +59,104 @@ function pushMessageToWSServer(currencies) {
 
 function pushValueToDashboardChart(chart, price, currencyValue) {
     const dataset = getDatasetByCurrencyName(chart, currencyValue)
-    if(typeof dataset !== "undefined") {
+    if (typeof dataset !== "undefined") {
         updateChart(chart, dataset, price)
     }
 }
 
 console.log("Chart initialization step")
-const chart = createChart() // chart is a global variable in our application
-console.log("Subscribe on all currencies. Default behavior")
+// TODO: Implement
+//const chart = createChart() // chart is a global variable in our application
+
+/*console.log("Subscribe on all currencies. Default behavior")
 subscribeOnCurrencies()
 console.log("Push message to the WS that we want this kind of data")
 pushMessageToWSServer(currencies)
+*/
+
+let src = 'https://corona.lmao.ninja/v2/historical?lastdays=30'
+
+// test 
+function subscribeCovid() {
+    var result = rxjs.from(fetch(src)
+        .then((response) => { return response.json() }))
+        .pipe(
+            switchMap((country) => country),
+            map(elem => [{ countryName: elem }]))
+    result.subscribe(
+        response => console.log(response), e => console.error(e)
+    )
+}
+
+var resp = null
+
+function currenciesList() {
+    var result = rxjs.from(fetch(src).then((response) => { return response.json() }))
+    result.subscribe(response => {
+        // resp = response.map(country => {})
+        console.log(response)
+    }, e => console.error(e))
+}
+
+currenciesList()
+//subscribeCovid()
+
+function loadGlobalStats() {
+    var result = rxjs.from(fetch(src).then((response) => { return response.json() }))
+        .pipe(removeUnnecessaryData(),
+            transformEachCountryDataToGetTotalNumbers(),
+            calculateGlobalStats())
+    result.subscribe(response => loadGlobalStatsDOMUpdate(response), e => console.error(e))
+}
+
+function loadGlobalStatsDOMUpdate(response) {
+    document.getElementById('totalCases').innerHTML = response.totalCases.toLocaleString()
+    document.getElementById('totalRecoveries').innerHTML = response.totalRecoveries.toLocaleString()
+    document.getElementById('totalDeaths').innerHTML = response.totalDeaths.toLocaleString()
+    console.log(response)
+}
+
+function loadCountries() { 
+    var result = rxjs.from(fetch(src).then((response) => { return response.json() }))
+        .pipe(removeUnnecessaryData(), map(obj => obj.map(country => country.countryName)))
+    result.subscribe(response => loadCountriesDOMUpdate(new Set(response)), e => console.error(e))
+}
+
+function loadCountriesDOMUpdate(response) {
+    response.forEach(countryName => {
+        var optCountry = document.createElement("option")
+        optCountry.value= countryName
+        optCountry.innerHTML = countryName
+        document.getElementById('countriesSelect').appendChild(optCountry)
+})
+}
+
+function calculateGlobalStats() {
+    return map(obj => {
+        const cases = obj.map(country => country.cases).reduce((cases1, cases2) => cases1 + cases2)
+        const recoveries = obj.map(country => country.recoveries).reduce((cases1, cases2) => cases1 + cases2)
+        const deaths = obj.map(country => country.deaths).reduce((cases1, cases2) => cases1 + cases2)
+        return ({ totalCases: cases, totalRecoveries: recoveries, totalDeaths: deaths })
+    })
+}
+
+function removeUnnecessaryData() {
+    return map(obj => obj.map(country => ({ countryName: country.country, countryTimeline: country.timeline })))
+}
+
+function transformEachCountryDataToGetTotalNumbers() {
+    return map(obj => obj.map(country => ({
+        countryName: country.countryName,
+        recoveries: Object.values(country.countryTimeline.recovered).slice(-1)[0],
+        cases: Object.values(country.countryTimeline.cases).slice(-1)[0],
+        deaths: Object.values(country.countryTimeline.deaths).slice(-1)[0]
+    })))
+}
+
+
+function entryPoint() {
+    loadGlobalStats()
+    loadCountries()
+}
+
+entryPoint()
